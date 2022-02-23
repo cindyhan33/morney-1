104b4c084c31872ffd0ddea7a9bc441196349233
cindy <283175003@qq.com>
2022-02-23 16:10:25 +0800

    deploy

---
+++ precache-manifest.3461aecb1fb63e564c430a4fe449a16f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c954cd9ac72c994d7919",
+    "url": "/morney-1/css/app.ff2f5de7.css"
+  },
+  {
+    "revision": "ceae25c2d275fb0fe64316fa50c3df27",
+    "url": "/morney-1/index.html"
+  },
+  {
+    "revision": "c954cd9ac72c994d7919",
+    "url": "/morney-1/js/app.84b2040d.js"
+  },
+  {
+    "revision": "0e70354f54bcfef9c634",
+    "url": "/morney-1/js/chunk-vendors.cee01823.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/morney-1/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/morney-1/robots.txt"
+  }
+]);
